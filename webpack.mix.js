caec26b5f6a26762d0d20446736ab494b8dfb27e
iamchristopher <chriswright@live.com>
2017-08-20 15:24:32 -0400

    Added image processing to build process

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,49 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminJpegoptim = require('imagemin-jpegoptim');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const {
+    join
+} = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .version();
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.webpackConfig({
+    plugins: [
+        new CopyWebpackPlugin([{
+            context: 'resources/assets/images',
+            from: '**/*',
+            to: 'images'
+        }]),
+        new ImageminPlugin({
+            disable: process.env.NODE_ENV !== 'production',
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            gifsicle: {
+                interlaced: true
+            },
+            optipng: {
+                optimizationLevel: 5
+            },
+            svgo: {
+                plugins: [
+                    {
+                        cleanupIDs: false,
+                        removeEmptyAttrs: false,
+                        removeViewBox: false
+                    }
+                ]
+            },
+            jpegtran: null,
+            plugins: [
+                ImageminJpegoptim({
+                    max: 85,
+                    progressive: true
+                })
+            ]
+        })
+    ]
+});
