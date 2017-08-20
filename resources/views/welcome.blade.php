38725dbf8ee10ea3330077a9219f8755f0d3a2e4
iamchristopher <chriswright@live.com>
2017-08-20 15:50:11 -0400

    Added link to main logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,19 @@
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css" />
     </head>
     <body>
-            <header class="mainHeader container">
+        <header class="mainHeader container">
+            <a href="/" title="Hilary Lucio Creative">
                 <img alt="Hilary Lucio Creative logo" src="/images/logo_main.png" />
-                <nav class="mainNavigation">
-                    <ul>
-                        <li><a href="#" title="See my work">Portfolio</a></li>
-                        <li><a href="#" title="View my resume">CV</a></li>
-                        <li><a href="#" title="Behold my wares">Services</a></li>
-                        <li><a href="#" title="Read aricles">Articles</a></li>
-                        <li><a href="#" title="Contact">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
+            </a>
+            <nav class="mainNavigation">
+                <ul>
+                    <li><a href="#" title="See my work">Portfolio</a></li>
+                    <li><a href="#" title="View my resume">CV</a></li>
+                    <li><a href="#" title="Behold my wares">Services</a></li>
+                    <li><a href="#" title="Read aricles">Articles</a></li>
+                    <li><a href="#" title="Contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
     </body>
 </html>
